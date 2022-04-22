a17bf0eb2d26a1a10e9fd3b976ddfa1ce68ef242
Tal Spektor <tal.s@zemingo.com>
2022-04-22 23:14:27 +0300

    Initial Commit

---
+++ Task Management Demo/Task_Management_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_Management_DemoApp.swift
+//  Task Management Demo
+//
+//  Created by Tal talspektor on 22/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Task_Management_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
